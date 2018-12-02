1a1630242e0fc10bfec6000de2b8c9fcb6056e5f
Logan <p.niezgoda@hotmail.com>
2018-12-02 11:10:29 +0100

    almost finished mobile view for home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 (function() {
-  const naviContainer = document.querySelector('#navigationMenuContainer');
-  const hamburgerBtn = document.querySelector('#hamburgerBtn');
+  const naviContainer = document.querySelector('#js-navigationMenuContainer');
+  const hamburgerBtn = document.querySelector('#js-hamburgerBtn');
 
   hamburgerBtn.addEventListener('click', () => {
     naviContainer.classList.toggle('menu-container--open');
